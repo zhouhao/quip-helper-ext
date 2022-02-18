5db7e6f3433f52c5ab9b78229351764fd23cccb7
Hao Zhou <hzhou@quip.com>
2022-02-18 11:25:46 -0800

    enable toggle zen mode by contextMenu

--- src/background.js
+++ src/background.js
@@ -1,11 +1,29 @@
 import * as types from './utils/action-types';
 
-chrome.commands.onCommand.addListener(command => {
-  if (command === types.CMD_QUIP_ZEN_MODE) {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: types.CMD_QUIP_ZEN_MODE }, response => {
-        console.log(response);
-      });
-    });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    title: 'Toggle Zen Mode',
+    id: types.MENU_QUIP_ZEN_MODE,
+    contexts: ['all'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === types.MENU_QUIP_ZEN_MODE) {
+    toggleZenMode();
   }
 });
+
+chrome.commands.onCommand.addListener(command => {
+  if (command === types.CMD_QUIP_ZEN_MODE) {
+    toggleZenMode();
+  }
+});
+
+const toggleZenMode = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: types.CMD_QUIP_ZEN_MODE }, response => {
+      console.log(response);
+    });
+  });
+};
--- src/utils/action-types.js
+++ src/utils/action-types.js
@@ -1,1 +1,3 @@
 export const CMD_QUIP_ZEN_MODE = 'QUIP_ZEN_MODE';
+
+export const MENU_QUIP_ZEN_MODE = 'MENU_QUIP_ZEN_MODE';
